5024702b68a3a7ed06ae977615e4b43db8a094e3
Stéphane Delecroix <s.delecroix@gmail.com>
2021-09-29 16:01:27 +0200

    feat(room): expose fetchRoomService in utils

--- src/services/utils/index.ts
+++ src/services/utils/index.ts
@@ -3,8 +3,11 @@
 import 'reflect-metadata';
 import { RightService } from './services/rightService/rightService';
 import { container } from 'tsyringe';
+import { FetchRoomService } from './services/fetchRoomService/fetchRoomService';
 
 const childContainer = container.createChildContainer();
+
 export const utils = {
-  rightService: childContainer.resolve(RightService)
+  rightService: childContainer.resolve(RightService),
+  fetchRoomService: childContainer.resolve(FetchRoomService)
 };
