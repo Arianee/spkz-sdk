f563f2db9de770689d0b2a6b75537e0f1d2c4d31
Stéphane Delecroix <s.delecroix@gmail.com>
2021-09-29 17:52:48 +0200

    fix(mumbai): change mumbai provider

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,8 +1,8 @@
 
 export const environment = {
-  defaultProvider: 'https://matic-mumbai.chainstacklabs.com',
+  defaultProvider: 'https://speedy-nodes-nyc.moralis.io/337ce5317ffe1f66093a6c3b/polygon/mumbai',
   roomContractAddress: '0x395bE7b1443b6c3Ce5177b2300E5cc20bF22576E',
-  chainId: '137',
+  chainId: '80001',
   networkdId: '1'
 
 };
